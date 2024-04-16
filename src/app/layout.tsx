3de5e0dcec1fa8bd9152056da2282457177087a3
joao nishimoto <jvonishimoto@gmail.com>
2024-04-16 10:32:31 -0300

    pt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="py-5 bg-teal-50">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
